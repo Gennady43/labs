0e84657020ffa8852f412d2d9cef8625ca356942
Gennady43 <79197898+Gennady43@users.noreply.github.com>
2021-05-24 16:38:18 +0300

    Add files via upload

---
+++ Lab 3568/Lab 3568/Sport.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab_3568
+{
+    enum Rank
+    {
+        Junior3 = 1,
+        Junior2,
+        Junior1,
+        Adult3,
+        Adult2,
+        Adult1,
+        CMS,
+        MS,
+        IMS,
+        HMS
+    }
+    abstract class Sport : Human
+    {
+        public int Age { get; set; }
+        public Rank @Rank { get; set; }
+
+        public Sport() : base()
+        {
+            Age = 23;
+            Rank = Rank.CMS;
+        }
+        public Sport(string nanem, string country, int age, Rank rank) : base(nanem, country)
+        {
+            Age = age;
+            Rank = rank;
+        }
+        public override string ToString() => base.ToString() + $"\nAge: {Age}\nRank: {Rank}";
+    }
+}
---
+++ Lab 3568/Lab 3568/Sportsman.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Lab_3568
+{
+    delegate void MyDelegate();
+    class Sportsman : Sport, IComparable, ICloneable, ICompetitions
+    {
+        public string Sport { get; set; }
+        public struct Competitions
+        {
+            public DateTime Date;
+            public string CompetitionsName;
+        }
+        public Competitions comp;
+        public Sportsman() : base()
+        {
+            Sport = "Cycling";
+            comp.Date = new DateTime(2021, 07, 14);
+            comp.CompetitionsName = "Group race";
+        }
+        public Sportsman(string name = "", string country = "", int age = 0, string sport = "", Rank rank = default, string competitionsname = "", DateTime date = default) : base(name, country, age, rank)
+        {
+            Sport = sport;
+            comp.Date = date;
+            comp.CompetitionsName = competitionsname;
+        }
+        public override string ToString() => base.ToString() + $"\nSport: {Sport}\nCompetitions: {comp.CompetitionsName}\t{comp.Date.ToString("d")}\n";
+        int IComparable.CompareTo(object obj)
+        {
+            if (this.Age > ((Sportsman)obj).Age) 
+                return 1;
+            if (this.Age < ((Sportsman)obj).Age) 
+                return -1;
+            else 
+                return 0;
+        }
+        public object Clone() => (Sportsman)this.MemberwiseClone();
+        public event MyDelegate Dope;
+        public void TakeDope()
+        {
+            Name = Country = ID = Sport = comp.CompetitionsName = "Disqualified";
+            Age = 0;
+            Rank = 0;
+            comp.Date = default;
+        }
+        public void Violation() => Dope?.Invoke();
+        public event MyDelegate Payment;
+        public void TooYoung(MyDelegate del) => del();
+        public void NotPaid()
+        {
+            Name = Country = ID = Sport = comp.CompetitionsName = "Not allowed";
+            Age = 0;
+            Rank = 0;
+            comp.Date = default;
+        }
+        public void NotAllowed() => Payment?.Invoke();
+    }
+}
